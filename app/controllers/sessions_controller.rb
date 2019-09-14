0d068936d63e08a267558b5db4e49adee6073029
Mario Adamo Jr <marioadamojr@gmail.com>
2019-09-14 15:49:40 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,6 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(username: params[:username])
     return head(:forbidden) unless @user.authenticate(params[:password])
-    session[:user_id] = @user.# IDEA: 
+    session[:user_id] = @user.id
   end
 end
