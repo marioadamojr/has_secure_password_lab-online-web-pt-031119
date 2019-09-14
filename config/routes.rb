0618c29ee5bca6745c82138f298c9ce5d9c118cb
Mario Adamo Jr <marioadamojr@gmail.com>
2019-09-14 15:55:28 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-  get '/homepage', to: '/users/homepage'
 
   resources :users
+  get '/homepage', to: '/users/homepage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
