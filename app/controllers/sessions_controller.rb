e7bd914689216d844b03d31f77ed43334f7e4e77
Mario Adamo Jr <marioadamojr@gmail.com>
2019-09-14 16:27:22 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,5 +6,6 @@ class SessionsController < ApplicationController
     @user = User.find_by(name: params[:user][:name])
     return head(:forbidden) unless @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
+    redirect_to '/homepage'
   end
 end
